9e432f87b2e894b248958fff334b4b4055303143
tdostilio <tdostilio@gmail.com>
2017-07-20 17:02:11 -0400

    finished coffee form exercise

--- scripts/main.js
+++ scripts/main.js
@@ -4,17 +4,36 @@ var $coffeeSize = $("[data-type='coffeeSize']");
 var $shotFlavor = $("[data-type='shotFlavor']");
 var $strengthLevel = $("[data-type='strengthLevel']");
 var $theForm = $('[data-coffee-order="form"]')
-// var recentLocalData = localStorage.getItem()
+var recentOrder = {};
+var Key = "";
+var Orders = [];
 
 //SUBMISSION
 $theForm.on('submit', function (event) {
+    event.preventDefault();
     localStorage.setItem('coffeeOrder', $coffeeOrder.val());
     localStorage.setItem('emailAddress', $emailAddress.val());
     localStorage.setItem('coffeeSize', $coffeeSize.val());
     localStorage.setItem('shotFlavor', $shotFlavor.val());
     localStorage.setItem('strengthLevel', $strengthLevel.val());
+    pullFromLocal();
+    storeOrder(recentOrder);
+    console.log(recentOrder);
+    console.log(Orders);
 });
 
+// PULL ORDER FROM LOCAL STORAGE
+function pullFromLocal() {
+    for (var i = 0; Key = window.localStorage.key(i); i++) {
+        recentOrder[Key] = window.localStorage.getItem(Key);
+    }
+    return recentOrder;
+};
+
+// STORE PULL INTO ARRAY OF OBJECTS;
+function storeOrder(recent) {
+    Orders.push(recent);
+}
 
 $(window).on('load', function (event) {
     $coffeeOrder.val(localStorage.getItem('coffeeOrder'));
@@ -23,14 +42,3 @@ $(window).on('load', function (event) {
     $shotFlavor.val(localStorage.getItem('shotFlavor'));
     $strengthLevel.val(localStorage.getItem('strengthLevel')); 
 });
-
-
-var i = 0,
-    Orders = {},
-    sKey;
-for (; sKey = window.localStorage.key(i); i++) {
-    Orders[sKey] = window.localStorage.getItem(sKey);
-}
-console.log(Orders);
-
-console.log(typeof Orders);
